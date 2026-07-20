092fc4e9034ad8b9a49c801838962f1957b5e967
chame <chame.murrieta@icloud.com>
2026-07-20 17:53:40 -0600

    20-07-26/2

--- modules/nixos/networking/wifi.nix
+++ modules/nixos/networking/wifi.nix
@@ -1,10 +1,23 @@
-{ ... }:
+{ lib, ... }:
 
 {
 
   networking = {
     networkmanager.wifi.backend = "iwd";
-    wireless.iwd.enable = true;
+    wireless = {
+	  iwd = {
+	    enable = true;
+		settings = {
+          General = {
+            AddressRandomization = "network";
+			EnableNetworkConfiguration = false;
+		  };
+	 	  Settings = {
+            AutoConnect = true; 
+		  };
+		};
+	  };
+	};
   };
 
 }
